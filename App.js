0907b66d9ea8f9441ff5824fe49abfa9ce630296
Kirill Borun <69752848+tabydabi@users.noreply.github.com>
2021-06-02 19:21:11 +0300

    add geo

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert } from 'react-native';
 import * as Location from 'expo-location';
 import Loading from './Loading';
 
 
+const apiKey = 'e7f78180f3b6842db147956350f8fab3'
+
+
 export default class extends React.Component {
 
+  state = {
+
+    isLoading: true
+  }
+
+  getLocation = async () =>{
+    try {
+         await Location.requestForegroundPermissionsAsync()
+        
+          const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync()
+          this.setState({isLoading: false}); 
+          //сделать запрос к API
+    } catch (error) {
+      Alert.alert('Не могу определить геопозицию');
+    }
+
+  }
+
+
+
   componentDidMount(){
+    this.getLocation();
 
     
   }
-
+ 
   render() {
+    const {isLoading} = this.state;
     return(
-   <Loading />
+     isLoading ? <Loading /> : null
     );
   };
 }
