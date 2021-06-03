307c2f22c4703ea2fae8e46e8f64e2bf0c7e5b4f
Kirill Borun <69752848+tabydabi@users.noreply.github.com>
2021-06-03 15:32:50 +0300

    end of logics

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Alert } from 'react-native';
 import * as Location from 'expo-location';
 import Loading from './Loading';
+import axios from 'axios';
 
 
 const apiKey = 'e7f78180f3b6842db147956350f8fab3'
@@ -15,13 +16,21 @@ export default class extends React.Component {
     isLoading: true
   }
 
+  getWeather = async (latitude, longitude) => {
+
+    const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
+    console.log(data);
+  }
+
   getLocation = async () =>{
     try {
          await Location.requestForegroundPermissionsAsync()
         
           const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync()
+          this.getWeather(latitude, longitude)
           this.setState({isLoading: false}); 
           //сделать запрос к API
+          
     } catch (error) {
       Alert.alert('Не могу определить геопозицию');
     }
