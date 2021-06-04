9d1f0ca6ec4d60cece21b1d88b69d6fce326e7f1
Kirill Borun <69752848+tabydabi@users.noreply.github.com>
2021-06-04 12:42:14 +0300

    End.

--- Weather.js
+++ Weather.js
@@ -1,54 +1,74 @@
 import React from 'react';
 import propTypes from 'prop-types'
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { LinearGradient} from 'expo-linear-gradient';
-import { Feather } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { LinearGradient} from 'expo-linear-gradient';
+
+
 
 
 const weatherOptions = {
 
-    Rain: {
-        iconName: 'rainy',
-        gradient: ['#000046','#1CB5E0']
-    },
     Thunderstorm: {
         iconName: 'weather-lightning',
-        gradient: ['#141E30', '#243B55']
+        gradient: ['#141E30', '#243B55'],
+        title: 'Сиди дома',
+        subtitle: 'Ты видишь что на улице?'
     },
     Drizzle: {
-        iconName: 'cloud-drizzle',
-        gradient: ['#3a7bd5', '#3a6073']
+        iconName: 'weather-rainy',
+        gradient: ['#3a7bd5', '#3a6073'],
+        title: 'Возьми зонтик',
+        subtitle: 'Возможно скоро дождь усилится '
+    },
+    Rain: {
+        iconName: 'weather-pouring',
+        gradient: ['#000046','#1CB5E0'],
+        title: 'На улице дождь',
+        subtitle: 'А значит скоро будет радуга!'
     },
     Snow: {
-        iconName: 'snow',
-        gradient: ['#83a4d4', '#b6fbff']
+        iconName: 'snowflake',
+        gradient: ['#83a4d4', '#b6fbff'],
+        title: 'На улице снежок!',
+        subtitle: 'Одевайтесь потеплее, лепите снеговиков'
     },
-    Fog: {
-        iconName: 'weather-fog',
-        gradient: ['#bdc3c7', '#2c3e50']
-    },
-    Mist: {
-        iconName: 'weather-fog',
-        gradient: ['#606c88', '#3f4c6b']
-    },
-    Clear: {
-        iconName: 'sunny',
-        gradient: ['#56CCF2', '#2F80ED']
-    },
-    Clouds: {
-        iconName: 'weather-cloudy',
-        gradient: ['#757F9A', '#D7DDE8']
-    },
-    Haze: {
-        iconName: 'weather-hazy',
-        gradient: ['#3E5151', '#DECBA4']
+    Dust: {
+        iconName: 'weather-windy-variant',
+        gradient: ['#B79891', '#94716B'],
+        title: 'Пыльно',
+        subtitle: 'Лучше закройте окна'
     },
     Smoke: {
         iconName: 'weather-windy',
-        gradient: ['#56CCF2', '#2F80ED']
-    }
+        gradient: ['#56CCF2', '#2F80ED'],
+        title: 'На улице смог :(',
+        subtitle: 'Не советую выходить без необходимости'
+    },
+    Haze: {
+        iconName: 'weather-hazy',
+        gradient: ['#3E5151', '#DECBA4'],
+        title: 'На улице снежок!',
+        subtitle: 'Одевайтесь потеплее, лепите снеговиков'
+    },
+    Mist: {
+        iconName: 'weather-fog',
+        gradient: ['#606c88', '#3f4c6b'],
+        title: 'Ни черта не видно в тумане',
+        subtitle: 'Зато как в Сайлент-Хилле :)'
+    },
+    Clear: {
+        iconName: 'weather-sunny',
+        gradient: ['#56CCF2', '#2F80ED'],
+        title: 'Погода супер :)',
+        subtitle: 'Иди гулять, хватит сидеть дома!'
+    },
+    Clouds: {
+        iconName: 'weather-cloudy',
+        gradient: ['#757F9A', '#D7DDE8'],
+        title: 'Облака',
+        subtitle: 'Возможны осадки'
+    },
 
 }
 
@@ -62,11 +82,13 @@ export default function weather({temp, condition}){
         style={styles.container}>
             <StatusBar barStyle="light-content" />
             <View style={styles.halfContainer}>
-                <Ionicons name={weatherOptions[condition].iconName} size={96} color = 'white'  /> 
+                <MaterialCommunityIcons name={weatherOptions[condition].iconName} size={96} color = 'white'  /> 
                 <Text style={styles.temp}>{temp}°</Text> 
             </View>
-            <View style={styles.halfContainer}>
-                
+            <View style={{...styles.halfContainer, ...styles.textContainer}}>
+                <Text style={styles.title}> {weatherOptions[condition].title}</Text>
+                <Text style={styles.subtitle}> {weatherOptions[condition].subtitle}</Text>
+
             </View>
         </LinearGradient>
         
@@ -84,17 +106,40 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+
     },
     halfContainer: {
 
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+
     },
     temp: {
         fontSize: 42,
         color: 'white'        
+    },
+    title: {
+        color: 'white',
+        fontSize: 44,
+        fontWeight: "300",
+        marginBottom: 10,
+        textAlign: 'left'
+
+    },
+    subtitle: {
+        color: 'white',
+        fontWeight: "600",
+        fontSize: 24,
+        textAlign: 'left'
+
+    },
+    textContainer: {
+
+        paddingHorizontal: 20,
+        alignItems: 'flex-start',
+        flex: 1,
+        justifyContent: 'center',
+
     }
 })
